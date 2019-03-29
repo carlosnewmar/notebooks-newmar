8594ca5117b044fab75b4271c29fd02e8ce3e5d4
Carlos Newmar <newmar@esss.co>
2019-03-29 14:03:15 -0300

    Update notebooks

--- finite_difference.py
+++ finite_difference.py
@@ -9,12 +9,12 @@ class FiniteDifference():
         right_boundary_condition=('Dirichlet', 20.),
         source_term_function=lambda x : 0. * x
     ):
-        self._length = 10.
+        self._length = 10. # m
         self._number_of_points = number_of_points
         self._delta_x = self._length / self._number_of_points
         self._x = np.linspace(0, self._number_of_points, self._number_of_points) * self._delta_x
-        self._left_temperature = 100.
-        self._right_temperature = 20.
+        self._left_temperature = left_boundary_condition[1]
+        self._right_temperature = right_boundary_condition[1]
         self._b = np.zeros(self._number_of_points)
         self._A = np.zeros((self._number_of_points, self._number_of_points))
         self._source_term_function = source_term_function
@@ -25,17 +25,21 @@ class FiniteDifference():
         
     def AssemblyLinearSystem(self):
         n = self._number_of_points
-        
+
+        # Central points
         self._A.flat[0:n*n:n+1] = -2.
-        self._A[0, 0] = 1
-        self._A[-1, -1] = 1
-        
         self._A.flat[n+2:n*n-n:n+1] = 1.
         self._A.flat[n+0:n*n-n:n+1] = 1.
 
+        # Boundary Condition
+        self._A[0, 0] = 1
+        self._A[-1, -1] = 1
+        
+        # Independent Term - Boundary Condition
         self._b[0] = self._left_temperature
         self._b[-1] = self._right_temperature
         
+        # Independent Term - Central points - Source
         self._b[1:-1] = self._SourceTerm(self._x[1:-1])
 
     def Solve(self):
